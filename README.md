b74a27706c6998694aeaca324bbb93846b87af2f
ozers <ozer.subasigmail.com>
2020-02-22 18:10:16 +0300

    added button events, changed widgets, fonts and colors.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # personal_card_flutter
-flutter application that includes personal card
+
+This Personal Card developed with Flutter.
+I used a lot of Widgets while developing it. I also use an "onTap()" method to actively use button click events. I imported "url_launcher" package for use the method.
+
+While doing this project, I learned a lot about the using of Widgets and the editing of the visuals thanks to the documentation and speeded myself.
+
+While I'm uploading the project to GitHub, sometimes I'm using Fork, terminal is also good option for that.
+
+There is a demo below,
+
+![Ozer's Personal Card Demo](demo/ozers_personal_card.gif)
--- lib/main.dart
+++ lib/main.dart
@@ -10,145 +10,158 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        primary: true,
+        backgroundColor: Colors.deepPurple,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('./images/ozer.png'),
-            ),
-            Text(
-              'Ozer SUBASI',
-              style: TextStyle(
-                fontFamily: 'ZhiMangXing',
-                fontSize: 50.0,
-                color: Colors.white70,
-                letterSpacing: 2,
-              ),
-            ),
-            Text(
-              'Software Developer',
-              style: TextStyle(
-                fontFamily: 'PatrickHandSC',
-                fontSize: 30.0,
-                color: Colors.white70,
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 20.0,
-                horizontal: 20.0,
-              ),
-              child: InkWell(
-                onTap: () async {
-                  if (await canLaunch("tel://+905392260314")) {
-                    await launch("tel://+905392260314");
-                  }
-                },
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 10.0,
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.perm_phone_msg,
-                        size: 30.0,
-                        color: Colors.blueGrey,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text('+90 (539) *** ** **',
-                          style: TextStyle(
-                            fontFamily: 'PatrickHandSC',
-                            fontSize: 25.0,
-                            color: Colors.blueGrey,
-                          )),
-                    ],
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('./images/ozer.png'),
+                ),
+                Text(
+                  'Ozer SUBASI',
+                  style: TextStyle(
+                    fontFamily: 'LobsterTwo',
+                    fontSize: 50.0,
+                    color: Colors.white,
+                    letterSpacing: 2,
                   ),
                 ),
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 20.0,
-                horizontal: 20.0,
-              ),
-              child: InkWell(
-                onTap: () async {
-                  if (await canLaunch("mailto:ozer.subasi@gmail.com")) {
-                    await launch("mailto:ozer.subasi@gmail.com");
-                  }
-                },
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 10.0,
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.contact_mail,
-                        size: 30.0,
-                        color: Colors.blueGrey,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text('Send Mail',
-                          style: TextStyle(
-                            fontFamily: 'PatrickHandSC',
-                            fontSize: 25.0,
-                            color: Colors.blueGrey,
-                          )),
-                    ],
+                Text(
+                  'Software Developer',
+                  style: TextStyle(
+                    fontFamily: 'Satisfy',
+                    fontSize: 30.0,
+                    color: Colors.white70,
                   ),
                 ),
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 20.0,
-                horizontal: 20.0,
-              ),
-              child: InkWell(
-                onTap: () async {
-                  if (await canLaunch("https://www.github.com/ozers")) {
-                    await launch("https://www.github.com/ozers");
-                  }
-                },
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 10.0,
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.developer_mode,
-                        size: 30.0,
-                        color: Colors.blueGrey,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text('Github Repository',
-                          style: TextStyle(
-                            fontFamily: 'PatrickHandSC',
-                            fontSize: 25.0,
-                            color: Colors.blueGrey,
-                          )),
-                    ],
+                SizedBox(
+                  height: 20.0,
+                  child: Divider(
+                    color: Colors.deepPurple[800],
                   ),
                 ),
-              ),
-            ),
-          ]),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                  ),
+                  child: InkWell(
+                    onTap: () async {
+                      if (await canLaunch("tel://+905392260314")) {
+                        await launch("tel://+905392260314");
+                      }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.perm_phone_msg,
+                          size: 30.0,
+                          color: Colors.deepPurple[600],
+                        ),
+                        title: Text(
+                          'Call Me',
+                          style: TextStyle(
+                            fontFamily: 'Tomorrow',
+                            fontSize: 25.0,
+                            color: Colors.deepPurple[400],
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Click to make call',
+                          style: TextStyle(
+                            fontFamily: 'Satisfy',
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20.0,
+                  ),
+                  child: InkWell(
+                    onTap: () async {
+                      if (await canLaunch("mailto:ozer.subasi@gmail.com")) {
+                        await launch("mailto:ozer.subasi@gmail.com");
+                      }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.contact_mail,
+                          size: 30.0,
+                          color: Colors.deepPurple[600],
+                        ),
+                        title: Text(
+                          'Send Mail',
+                          style: TextStyle(
+                            fontFamily: 'Tomorrow',
+                            fontSize: 25.0,
+                            color: Colors.deepPurple[400],
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Click to send mail',
+                          style: TextStyle(
+                            fontFamily: 'Satisfy',
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                  ),
+                  child: InkWell(
+                    onTap: () async {
+                      if (await canLaunch("https://www.github.com/ozers")) {
+                        await launch("https://www.github.com/ozers");
+                      }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.developer_mode,
+                          size: 30.0,
+                          color: Colors.deepPurple[600],
+                        ),
+                        title: Text('Github Repository',
+                            style: TextStyle(
+                              fontFamily: 'Tomorrow',
+                              fontSize: 25.0,
+                              color: Colors.deepPurple[400],
+                            )),
+                        subtitle: Text(
+                          'Click to look into my GitHub Repository',
+                          style: TextStyle(
+                              fontFamily: 'Satisfy',
+                              color: Colors.blueGrey),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
         ),
       ),
     );
